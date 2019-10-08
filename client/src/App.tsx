03adb7a4898e81c51343f3a56f380ca5a5c71fb8
Besian <kodrab@gmatc.matc.edu>
2019-10-07 20:19:49 -0500

    Started Activity 4

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          BlogBox
+      </header>
+      </div>
+    );
+  }
+}
+
+export default App;
